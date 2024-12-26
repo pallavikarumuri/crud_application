028cb9464d3e34b6054fe981e4465a891e9db858
pallavikarumuri <146340470+pallavikarumuri@users.noreply.github.com>
2024-12-26 16:06:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Crud from "./coments/crud/crudpj"
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div>
+   
+    <Crud/>
+    </div>
+  );
+}
+export default App;
